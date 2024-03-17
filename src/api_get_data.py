d7c6d44948d5b76827ca14ade28d9fc74fb62200
Jorge Montañez Aguilar <149834282+Jorgemontanez25@users.noreply.github.com>
2024-03-17 23:51:51 +0000

    commit

--- src/api_get_data.py
+++ src/api_get_data.py
@@ -1,7 +1,4 @@
-#Esto es una prueba de control de versiones. Marzo 17, 3:11pm
-
-
-import csv
+#Esto es una prueba de control de versiones. Marzo 17, 17:49pm MX
 import os
 import datetime
 import requests
@@ -13,9 +10,9 @@ def write_to_log(message):
 
 # Function to delete rows with more fields than expected from a CSV file
 def delete_invalid_rows(csv_file, expected_fields):
-    # Read the contents of the CSV file into a list of lists
+    # Read the contents of the CSV file using DictReader
     with open(csv_file, 'r', newline='', encoding='utf-8') as file:
-        reader = csv.reader(file)
+        reader = csv.DictReader(file)
         rows = list(reader)
 
     # Filter out rows with more fields than expected
@@ -23,7 +20,8 @@ def delete_invalid_rows(csv_file, expected_fields):
 
     # Write the valid rows back to the CSV file
     with open(csv_file, 'w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
+        writer = csv.DictWriter(file, fieldnames=reader.fieldnames)
+        writer.writeheader()
         writer.writerows(valid_rows)
 
     print("Invalid rows deleted successfully")
@@ -37,31 +35,34 @@ params = {
     "fuel_type": "ELEC",  # Filter by electric stations
     "country": "US",
     "limit": "all"  # Get all results
- }
+}
 
 # Make the API request
 response = requests.get(url, params=params)
 
+# Print the response status code
+print(f"Response status code: {response.status_code}")
+
 # Check if the request was successful
 if response.status_code == 200:
     # Define the directory to save the files
-    directory = '/workspaces/Machine-Learning-Final-Project/data/raw'
+    directory = 'data/raw'
     os.makedirs(directory, exist_ok=True)
 
     # Write the response content to a CSV file in the specified directory
     csv_file_path = os.path.join(directory, "electric_stations.csv")
-    with open(csv_file_path, "w", encoding="utf-8") as csv_file:
-        csv_file.write(response.text)
+    print(f"CSV file path: {csv_file_path}")
+    try:
+        with open(csv_file_path, "wb") as csv_file:
+            csv_file.write(response.content)
 
-    print(f"Data stored in {csv_file_path}")
-    write_to_log("API request completed successfully")
+        print(f"Data stored in {csv_file_path}")
+        write_to_log("API request completed successfully")
 
-    # Perform additional operations if needed
-    # For example, deleting invalid rows
-    delete_invalid_rows(csv_file_path, 77)
+    except Exception as e:
+        print(f"Error writing data to CSV file: {e}")
 else:
     print(f"Error fetching data: {response.status_code}")
     print("Error message:", response.text)
     write_to_log(f"Error fetching data: {response.status_code}. Error message: {response.text}")
-
 
